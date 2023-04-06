36e6c1cc4c9b325d2c6f713e0b97b4972e7bd30b
Jan Juracka <janjuracka861@seznam.cz>
2023-04-06 14:43:33 +0200

    ADD: Vector3 c'tor tests

--- tests/vector3.cxx
+++ tests/vector3.cxx
@@ -1,19 +1,54 @@
 #include "../src/quaternion/vector3.hpp"
+#include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <tuple>
 
-TEST_CASE("One value c'tor") {
-	float val = 1.f;
-	q::Vector3 vec(val);
+using floatTypes = std::tuple<float, double>;
 
-	auto x = vec.x();
-	auto y = vec.y();
-	auto z = vec.z();
+TEMPLATE_LIST_TEST_CASE("Constructors", "[construtor][template]", floatTypes) {
+	SECTION("One-value c'tor") {
+		TestType val = 1.f;
+		q::Vector3 vec(val);
 
-	REQUIRE(std::is_same_v<decltype(x), float>);
-	REQUIRE(std::is_same_v<decltype(y), float>);
-	REQUIRE(std::is_same_v<decltype(z), float>);
+		auto x = vec.x();
+		auto y = vec.y();
+		auto z = vec.z();
 
-	REQUIRE(x == val);
-	REQUIRE(y == val);
-	REQUIRE(z == val);
+		REQUIRE(std::is_same_v<decltype(x), TestType>);
+		REQUIRE(std::is_same_v<decltype(y), TestType>);
+		REQUIRE(std::is_same_v<decltype(z), TestType>);
+
+		REQUIRE(x == val);
+		REQUIRE(y == val);
+		REQUIRE(z == val);
+	}
+
+	SECTION("Three-value c'tor") {
+		TestType val_x = 1.f, val_y = 2.f, val_z = 3.f;
+		q::Vector3 vec(val_x, val_y, val_z);
+
+		auto x = vec.x();
+		auto y = vec.y();
+		auto z = vec.z();
+
+		REQUIRE(std::is_same_v<decltype(x), TestType>);
+		REQUIRE(std::is_same_v<decltype(y), TestType>);
+		REQUIRE(std::is_same_v<decltype(z), TestType>);
+
+		REQUIRE(x == val_x);
+		REQUIRE(y == val_y);
+		REQUIRE(z == val_z);
+	}
+
+	SECTION("C'tor equality") {
+		TestType val = 1.f;
+
+		q::Vector3 vec_a(val, val, val);
+		q::Vector3 vec_b(val);
+
+		REQUIRE(vec_a.x() == vec_b.x());
+		REQUIRE(vec_a.y() == vec_b.y());
+		REQUIRE(vec_a.z() == vec_b.z());
+		REQUIRE(vec_a == vec_b);
+	}
 }
