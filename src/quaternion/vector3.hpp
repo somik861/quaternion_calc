d28caea6dcc91624ef4b0b6b83d0751df42e0f1c
Jan Juracka <janjuracka861@seznam.cz>
2023-04-27 09:16:07 +0200

    ADD: Basic math operations to vector

--- src/quaternion/vector3.hpp
+++ src/quaternion/vector3.hpp
@@ -24,6 +24,19 @@ class Vector3 {
 	constexpr T& z() { return _values[2]; }
 	constexpr T z() const { return _values[2]; }
 
+	constexpr Vector3& transform(auto unary_op) {
+		std::ranges::transform(_values, _values.begin(), unary_op);
+		return *this;
+	}
+
+	constexpr Vector3 transformed(auto unary_op) {
+		Vector3 cpy = *this;
+		cpy.transform(unary_op);
+		return cpy;
+	}
+
+	// ============= basic operators =============
+
 	constexpr T& operator[](std::size_t idx) {
 		assert(idx < 3);
 		return _values[idx];
@@ -34,19 +47,62 @@ class Vector3 {
 		return _values[idx];
 	}
 
+	constexpr friend std::ostream& operator<<(std::ostream& os,
+	                                          const Vector3& v) {
+		return os << fmt::format("[{}, {}, {}]", v.x(), v.y(), v.z());
+	}
+
 	constexpr friend bool operator==(const Vector3& lhs, const Vector3& rhs) {
 		using details::isclose;
 
 		return isclose(lhs.x(), rhs.x()) && isclose(lhs.y(), rhs.y()) &&
 		       isclose(lhs.z(), rhs.z());
 	}
+	// ============= math operators =============
+	constexpr friend Vector3 operator+(const Vector3& lhs, const Vector3& rhs) {
+		return lhs._elem_wised(rhs, std::plus{});
+	}
+	constexpr friend Vector3 operator-(const Vector3& lhs, const Vector3& rhs) {
+		return lhs._elem_wised(rhs, std::minus{});
+	}
+	constexpr friend Vector3 operator*(const Vector3& lhs, const Vector3& rhs) {
+		return lhs._elem_wised(rhs, std::multiplies{});
+	}
+	constexpr friend Vector3 operator/(const Vector3& lhs, const Vector3& rhs) {
+		return lhs._elem_wised(rhs, std::divides{});
+	}
+	constexpr friend Vector3 operator-(const Vector& v) {
+		return v.transformed([](auto x) { return -x; });
+	}
 
-	constexpr friend std::ostream& operator<<(std::ostream& os,
-	                                          const Vector3& v) {
-		return os << fmt::format("[{}, {}, {}]", v.x(), v.y(), v.z());
+	constexpr Vector3& operator+(const Vector3& rhs) {
+		return _elem_wise(rhs, std::plus{});
+	}
+	constexpr Vector3& operator-(const Vector3& rhs) {
+		return _elem_wise(rhs, std::minus{});
+	}
+	constexpr Vector3& operator*(const Vector3& rhs) {
+		return _elem_wise(rhs, std::multiplies{});
+	}
+	constexpr Vector3& operator/(const Vector3& rhs) {
+		return _elem_wise(rhs, std::divides{});
 	}
 
   private:
+	constexpr Vector3& _elem_wise(const Vector3& rhs, auto binary_op) {
+		std::ranges::transform(_values, rhs._values, _values.begin(),
+		                       binary_op);
+		return *this;
+	}
+
+	constexpr friend Vector3
+	_elem_wised(const Vector3& lhs, const Vector3& rhs, auto binary_op) {
+		Vectro3 cpy = lhs;
+		cpy._elem_wise(rhs, binary_op);
+		return cpy;
+	}
+
 	std::array<T, 3> _values;
 };
+
 } // namespace q
