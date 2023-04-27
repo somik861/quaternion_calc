298e92a32e6c81fee15faedb116af1bfbd8cb58b
Jan Juracka <janjuracka861@seznam.cz>
2023-04-27 17:46:13 +0200

    ADD: Math to quaternion (tests still needed)

--- src/quaternion/quaternion.hpp
+++ src/quaternion/quaternion.hpp
@@ -2,22 +2,51 @@
 
 #include "details.hpp"
 #include "vector3.hpp"
+#include <cmath>
 
 namespace q {
 template <typename T>
 class Quaternion {
   public:
-	Quaternion(T real, const Vector3<T>& imag) : _real(real), _imag(imag) {}
-	Quaternion(T x, T i, T j, T k) : Quaternion(x, {i, j, k}) {}
+	Quaternion(T real, const Vector3<T>& imag) noexcept
+	    : _real(real), _imag(imag) {}
+	Quaternion(T x, T i, T j, T k) noexcept : Quaternion(x, {i, j, k}) {}
 
-	constexpr T& real() { return _real; }
-	constexpr T real() const { return _real; }
+	constexpr T& real() noexcept { return _real; }
+	constexpr T real() const noexcept { return _real; }
 
-	constexpr Vector3<T>& imag() { return _imag; }
-	constexpr const Vector3<T>& imag() const { return _imag; }
+	constexpr Vector3<T>& imag() noexcept { return _imag; }
+	constexpr const Vector3<T>& imag() const noexcept { return _imag; }
+
+	T norm() const noexcept {
+		return std::sqrt(_real * _real + dot(_imag, _imag));
+	};
+
+	constexpr Quaternion& conjugate() noexcept {
+		_imag = -_imag;
+		return *this;
+	}
+
+	constexpr Quaternion& conjugated() const noexcept {
+		auto cpy = *this;
+		return cpy.conjugate();
+	}
+
+	Quaternion& reciprocal() noexcept {
+		conjugate();
+		(*this) *= (1 / std::pow(norm(), T(2)));
+		return *this;
+	}
+
+	Quaternion reciprocaled() const noexcept {
+		auto cpy = *this;
+		return cpy.reciprocal();
+	}
+
+	// ============= basic operators =============
 
 	constexpr friend bool operator==(const Quaternion& lhs,
-	                                 const Quaternion& rhs) {
+	                                 const Quaternion& rhs) noexcept {
 		using details::isclose;
 
 		return isclose(lhs.real(), rhs.real()) && lhs.imag() == rhs.imag();
@@ -27,6 +56,57 @@ class Quaternion {
 	                                          const Quaternion& q) {
 		return os << fmt::format("Q({}, {})", q.real(),
 		                         details::to_string(q.imag()));
+	}
+
+	// ============= math operators =============
+	constexpr friend Quaternion operator+(const Quaternion& lhs,
+	                                      const Quaternion& rhs) noexcept {
+		return {lhs.real() + rhs.real(), lhs.imag() + rhs.imag()};
+	}
+
+	constexpr friend Quaternion operator-(const Quaternion& lhs,
+	                                      const Quaternion& rhs) noexcept {
+		return {lhs.real() - rhs.real(), lhs.imag() - rhs.imag()};
+	}
+
+	constexpr friend Quaternion operator*(T lhs,
+	                                      const Quaternion& rhs) noexcept {
+		return {lhs * rhs.real(), lhs * rhs.imag()};
+	}
+
+	constexpr friend Quaternion operator*(const Quaternion& lhs,
+	                                      T rhs) noexcept {
+		return {lhs.real() * rhs, lhs.imag() * rhs};
+	}
+
+	constexpr friend Quaternion operator-(const Quaternion& op) {
+		return {-op.real(), -op.imag()};
+	}
+
+	constexpr friend Quaternion operator*(const Quaternion& lhs,
+	                                      const Quaternion& rhs) noexcept {
+		T real = lhs.real() * rhs.real() - dot(lhs.imag(), rhs.imag());
+		Vector3<T> imag = cross(lhs.imag(), rhs.imag()) +
+		                  lhs.real() * rhs.imag() + rhs.real() * lhs.imag();
+		return {real, imag};
+	}
+
+	constexpr Quaternion& operator+=(const Quaternion& rhs) noexcept {
+		_real += rhs.real();
+		_imag += rhs.imag();
+		return *this;
+	}
+
+	constexpr Quaternion& operator-=(const Quaternion& rhs) noexcept {
+		_real -= rhs.real();
+		_imag -= rhs.imag();
+		return *this;
+	}
+
+	constexpr Quaternion& operator*=(T rhs) noexcept {
+		_real *= rhs;
+		_imag *= rhs;
+		return *this;
 	}
 
   private:
