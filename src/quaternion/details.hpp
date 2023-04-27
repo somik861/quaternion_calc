51450cbe70804f36733d06066ac87af322b7b31c
Jan Juracka <janjuracka861@seznam.cz>
2023-04-27 11:11:21 +0200

    CHG: fabs -> abs

--- src/quaternion/details.hpp
+++ src/quaternion/details.hpp
@@ -1,15 +1,16 @@
 #pragma once
 
 #include "concepts.hpp"
+#include <cmath>
 #include <concepts>
 #include <limits>
 
 namespace q::details {
 template <std::floating_point T>
 constexpr bool isclose(T a, T b) {
-	return std::fabs(a - b) <=
-	           std::numeric_limits<T>::epsilon() * std::fabs(a + b) * 4 ||
-	       std::fabs(a - b) <= std::numeric_limits<T>::min();
+	return std::abs(a - b) <=
+	           std::numeric_limits<T>::epsilon() * std::abs(a + b) * 4 ||
+	       std::abs(a - b) <= std::numeric_limits<T>::min();
 }
 
 template <std::integral T>
