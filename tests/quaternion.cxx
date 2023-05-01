3de107940be9df180c3dd054ccbf6feb5c086a5e
Jan Juracka <janjuracka861@seznam.cz>
2023-05-01 13:40:41 +0200

    ADD: Quaternion tests

--- tests/quaternion.cxx
+++ tests/quaternion.cxx
@@ -42,4 +42,46 @@ TEMPLATE_LIST_TEST_CASE("Quaternion", "Quaternion[template]", floatTypes) {
 		REQUIRE(to_string(_vec_quat) ==
 		        fmt::format("Q({}, {})", _real, to_string(_imag)));
 	}
+
+	SECTION("Math") {
+		using T = TestType;
+		using quat_t = q::Quaternion<T>;
+		quat_t quat_1(1.f, 1.f, 1.f, 1.f);
+		quat_t quat_2(2.f, 2.f, 2.f, 2.f);
+
+		REQUIRE(_vec_quat.conjugated() == quat_t(1.f, -2.f, -3.f, -4.f));
+		auto cpy = _vec_quat;
+		cpy.conjugate();
+		REQUIRE(cpy == quat_t(1.f, -2.f, -3.f, -4.f));
+
+		REQUIRE(quat_1.reciprocaled() == quat_t(0.25f, -0.25f, -0.25f, -0.25f));
+
+		cpy = quat_2;
+		cpy.reciprocal();
+		REQUIRE(cpy == quat_t(0.125f, -0.125f, -0.125f, -0.125f));
+
+		REQUIRE(isclose(quat_1.norm(), T(2)));
+		REQUIRE(isclose(quat_2.norm(), T(4)));
+
+		REQUIRE(quat_1 + quat_2 == quat_t(3.f, 3.f, 3.f, 3.f));
+		REQUIRE(quat_2 - quat_1 == quat_t(1.f, 1.f, 1.f, 1.f));
+		REQUIRE(2 * quat_2 == quat_t(4.f, 4.f, 4.f, 4.f));
+		REQUIRE(quat_1 * 3 == 3 * quat_1);
+		REQUIRE(-quat_1 == quat_t(-1.f, -1.f, -1.f, -1.f));
+		REQUIRE(-quat_2 == quat_t(-2.f, -2.f, -2.f, -2.f));
+		REQUIRE(quat_1 * quat_2 == quat_t(-4.f, 4.f, 4.f, 4.f));
+
+		cpy = quat_1;
+		cpy += quat_2;
+		REQUIRE(cpy == quat_t(3.f, 3.f, 3.f, 3.f));
+
+		cpy -= quat_1;
+		REQUIRE(cpy == quat_t(2.f, 2.f, 2.f, 2.f));
+
+		cpy *= T(2);
+		REQUIRE(cpy == quat_t(4.f, 4.f, 4.f, 4.f));
+
+		cpy *= quat_2;
+		REQUIRE(cpy == quat_t(-16.f, 16.f, 16.f, 16.f));
+	}
 }
