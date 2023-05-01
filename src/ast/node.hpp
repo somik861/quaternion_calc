6467414e9ff2276429448c113a271ab3ed744201
Jan Juracka <janjuracka861@seznam.cz>
2023-05-01 16:10:49 +0200

    ADD: INode and Scalar node

---
+++ src/ast/node.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include "../quaternion/quaternion.hpp"
+#include <concepts>
+#include <variant>
+
+namespace ast {
+namespace node {
+template <std::floating_point T>
+class INode {
+  public:
+	using scalar_t = T;
+	using vector_t = q::Vector3<T>;
+	using quaternion_t = q::Quaternion<T>;
+	using result_type = std::variant<T, vector_t, quaternion_t>;
+
+	virtual constexpr result_type evaluate() const = 0;
+};
+
+template <std::floating_point T>
+class Scalar : public INode<T> {
+  public:
+	constexpr Scalar(T value) : _value(value) {}
+
+	constexpr INode<T>::result_type evaluate() const override { return _value; }
+
+  private:
+	T _value;
+};
+
+// TODO build from Scalars
+template <std::floating_point T>
+class Vector : public INode<T> {
+  public:
+	constexpr INode<T>::result_type evaluate() const override { return T(1); }
+
+  private:
+};
+
+} // namespace node
+} // namespace ast
--- tests/quaternions/empty.cxx
+++
@@ -1,6 +0,0 @@
-#include "../common.hxx"
-
-TEST_CASE("EMPTY") {
-	static_assert(true);
-	REQUIRE(true == true);
-}
