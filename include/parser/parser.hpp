b142e2ba7c1fec89aeab2834673616b4d641db24
Jan Juracka <janjuracka861@seznam.cz>
2023-05-08 12:43:21 +0200

    CHG: grammar improvements; ADD: parse scalar functionality

--- include/parser/parser.hpp
+++ include/parser/parser.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <ast/node.hpp>
 #include <boost/container/flat_set.hpp>
+#include <boost/lexical_cast.hpp>
 #include <fmt/core.h>
+#include <sstream>
 #include <stdexcept>
 #include <string_view>
 
@@ -16,40 +18,90 @@ class Parser {
 		_buffer = sv;
 		_original_size = sv.size();
 
+		_skip_whitespaces();
 		uptr_t rv = _parse_elem();
+		_skip_whitespaces();
 
-		sv = {};
+		if (_available())
+			throw std::logic_error(fmt::format(
+			    "Expected end of the line at position {}", _current_idx()));
+
+		sv = "";
 		return rv;
 	}
 
   private:
 	/* ======= parser logic ======== */
-	uptr_t _parse_elem() { return nullptr; }
+	uptr_t _parse_elem() {
+		char ch = _peek();
 
-	constexpr void skip_whitespaces() {
-		while (WHITESPACES.contains(_peek()))
+		if (DIGITS.contains(ch))
+			return _parse_scalar();
+		if (ch == '[')
+			return nullptr; // parse vector
+		if (ch == 'Q')
+			return nullptr; // parse quaternion
+		return nullptr;     // parse operation
+	}
+
+	uptr_t _parse_scalar() {
+		bool dot_seen = false;
+		std::string str;
+
+		char ch = _peek();
+		while (DIGITS.contains(ch) && (ch == '.' && !dot_seen)) {
+			if (ch == '.')
+				dot_seen = true;
+			str.push_back(ch);
+			_skip();
+		}
+
+		return ast::node::Scalar<T>::make_unique(boost::lexical_cast<T>(str));
+	}
+
+	void _skip_whitespaces() {
+		while (_available() && WHITESPACES.contains(_peek()))
 			_skip();
 	}
 
 	/* ======= basic operations ======== */
-	constexpr char _peek() const { return _buffer[0]; }
-	constexpr void _skip(int n = 1) { _buffer.remove_prefix(n); }
+	constexpr char _peek() const {
+		_require_available();
+		return _buffer[0];
+	}
+	constexpr void _skip(int n = 1) {
+		_require_available();
+		_buffer.remove_prefix(n);
+	}
 	constexpr void _require(char ch) {
+		_require_available();
 		if (_peek() != ch)
 			throw std::logic_error(
 			    fmt::format("Expected '{}' at position {}, got '{}'", ch,
-			                _original_size - _buffer.size(), _peek()));
+			                _current_idx(), _peek()));
 		_skip();
 	}
+	constexpr bool _available() const { return !_buffer.empty(); }
+	constexpr void _require_available() const {
+		if (!_available())
+			throw std::logic_error(
+			    fmt::format("Unexpected end at position {}", _current_idx()));
+	}
 	constexpr char _consume() {
+		_require_available();
 		char rv = _peek();
 		_skip();
 		return rv;
+	}
+	constexpr std::size_t _current_idx() const {
+		return _original_size - _buffer.size();
 	}
 
 	std::string_view _buffer;
 	std::size_t _original_size;
 	const static inline boost::container::flat_set<char> WHITESPACES{
 	    ' ', '\n', '\t', '\r', '\f', '\b'};
+	const static inline boost::container::flat_set<char> DIGITS{
+	    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 };
 } // namespace parser
